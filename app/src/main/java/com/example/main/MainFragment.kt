7739d8e9c7f77283e4ae7e3ce073fe4c89bbe853
wodnd <wodnd0418@naver.com>
2023-09-11 00:15:13 +0900

    design: main layout

---
+++ app/src/main/java/com/example/main/MainFragment.kt
@@ -0,0 +1,23 @@
+package com.example.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.main.databinding.FragmentMainBinding
+
+class MainFragment: Fragment() {
+
+    lateinit var binding: FragmentMainBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+}
