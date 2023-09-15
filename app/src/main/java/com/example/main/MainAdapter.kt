8c99b7fd8fc4e4f9e2c6383a622373e109537ecc
wodnd <wodnd0418@naver.com>
2023-09-16 07:42:49 +0900

    feat: heart click listener

--- app/src/main/java/com/example/main/MainAdapter.kt
+++ app/src/main/java/com/example/main/MainAdapter.kt
@@ -1,8 +1,14 @@
 package com.example.main
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.main.databinding.ItemBookBinding
@@ -16,7 +22,34 @@ class MainAdapter : RecyclerView.Adapter<MainAdapter.ViewHolder>(){
     }
 
     class ViewHolder(val binding: ItemBookBinding, val context: Context) : RecyclerView.ViewHolder(binding.root){
+
+
+        // Drawable 객체를 비교하는 함수
+        fun areDrawablesEqual(drawable1: Drawable, drawable2: Drawable): Boolean {
+            val bitmap1 = drawableToBitmap(drawable1)
+            val bitmap2 = drawableToBitmap(drawable2)
+            return bitmap1.sameAs(bitmap2)
+        }
+
+
+        // Drawable을 Bitmap으로 변환하는 함수
+        fun drawableToBitmap(drawable: Drawable): Bitmap {
+            if (drawable is BitmapDrawable) {
+                return drawable.bitmap
+            }
+
+            val width = drawable.intrinsicWidth
+            val height = drawable.intrinsicHeight
+
+            val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+            val canvas = Canvas(bitmap)
+            drawable.setBounds(0, 0, canvas.width, canvas.height)
+            drawable.draw(canvas)
+
+            return bitmap
+        }
         fun bind(item: Book){
+
             with(binding){
                 titleTv.text = item.title
                 if(item.heart.toString() == "true"){
@@ -25,6 +58,21 @@ class MainAdapter : RecyclerView.Adapter<MainAdapter.ViewHolder>(){
                 else{
                     heartIv.setImageResource(R.drawable.blank_heart)
                 }
+
+                binding.heartIv.setOnClickListener {
+                    val currentImg = binding.heartIv.drawable
+                    val heart = ContextCompat.getDrawable(context, R.drawable.heart)
+                    val blankheart = ContextCompat.getDrawable(context, R.drawable.blank_heart)
+                    if(currentImg != null && heart != null && blankheart != null){
+                        if(areDrawablesEqual(currentImg, heart)){
+                            binding.heartIv.setImageResource(R.drawable.blank_heart)
+                        }
+                        else if(areDrawablesEqual(currentImg, blankheart)){
+                            binding.heartIv.setImageResource(R.drawable.heart)
+                        }
+                    }
+                }
+
                 keywordRv.apply {
                     adapter = KeywordAdapter().build(item.keywords)
                     layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
@@ -32,6 +80,7 @@ class MainAdapter : RecyclerView.Adapter<MainAdapter.ViewHolder>(){
             }
         }
     }
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainAdapter.ViewHolder = ViewHolder(
         ItemBookBinding.inflate(LayoutInflater.from(parent.context), parent, false), parent.context)
@@ -41,6 +90,4 @@ class MainAdapter : RecyclerView.Adapter<MainAdapter.ViewHolder>(){
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind(items[position])
     }
-
-
 }
