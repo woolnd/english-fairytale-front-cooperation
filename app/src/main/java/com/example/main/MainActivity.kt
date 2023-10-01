b0249e6e708f454bbc22282434011288c86e64a3
wodnd <wodnd0418@naver.com>
2023-10-01 20:52:09 +0900

    feat: my page and popup

--- app/src/main/java/com/example/main/MainActivity.kt
+++ app/src/main/java/com/example/main/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.main
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
@@ -17,5 +18,10 @@ class MainActivity: AppCompatActivity() {
         window.statusBarColor = ContextCompat.getColor(this, R.color.white)
         supportFragmentManager.beginTransaction().add(R.id.main_fl, MainFragment())
             .commit()
+
+        binding.profileIv.setOnClickListener {
+            val intent = Intent(this, MypageActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
