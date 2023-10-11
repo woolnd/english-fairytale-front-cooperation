a0c1c3f2bec1586bdba9ad1fd7bfb065021d492e
wodnd <wodnd0418@naver.com>
2023-10-11 10:11:58 +0900

    feat: initial

---
+++ app/src/main/java/com/example/main/InitialActivity.kt
@@ -0,0 +1,82 @@
+package com.example.main
+
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.example.main.databinding.ActivityInitialBinding
+
+class InitialActivity: AppCompatActivity() {
+    lateinit var binding: ActivityInitialBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityInitialBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        window.statusBarColor = ContextCompat.getColor(this, R.color.white)
+
+//        Handler(Looper.getMainLooper()).postDelayed({
+//            val currentImg = binding.backgroundIv.drawable
+//            val back1 = ContextCompat.getDrawable(this, R.drawable.initial_back1)
+//            val back2 = ContextCompat.getDrawable(this, R.drawable.initial_back2)
+//            if (currentImg != null && back1 != null && back2 != null) {
+//                if (areDrawablesEqual(currentImg, back1)) {
+//                    binding.backgroundIv.setImageResource(R.drawable.initial_back2)
+//                } else if (areDrawablesEqual(currentImg, back2)) {
+//                    binding.backgroundIv.setImageResource(R.drawable.initial_back1)
+//                }
+//            }
+//        }, 3000)
+
+        val handler = Handler(Looper.getMainLooper())
+
+        val runnable = object : Runnable {
+            override fun run() {
+                val currentImg = binding.backgroundIv.drawable
+                val back1 = ContextCompat.getDrawable(this@InitialActivity, R.drawable.initial_back1)
+                val back2 = ContextCompat.getDrawable(this@InitialActivity, R.drawable.initial_back2)
+                if (currentImg != null && back1 != null && back2 != null) {
+                    if (areDrawablesEqual(currentImg, back1)) {
+                        binding.backgroundIv.setImageResource(R.drawable.initial_back2)
+                    } else if (areDrawablesEqual(currentImg, back2)) {
+                        binding.backgroundIv.setImageResource(R.drawable.initial_back1)
+                    }
+                }
+                handler.postDelayed(this, 3000)
+            }
+        }
+
+// 처음 실행 예약
+        handler.postDelayed(runnable, 3000)
+    }
+
+    fun areDrawablesEqual(drawable1: Drawable, drawable2: Drawable): Boolean {
+        val bitmap1 = drawableToBitmap(drawable1)
+        val bitmap2 = drawableToBitmap(drawable2)
+        return bitmap1.sameAs(bitmap2)
+    }
+
+
+    // Drawable을 Bitmap으로 변환하는 함수
+    fun drawableToBitmap(drawable: Drawable): Bitmap {
+        if (drawable is BitmapDrawable) {
+            return drawable.bitmap
+        }
+
+        val width = drawable.intrinsicWidth
+        val height = drawable.intrinsicHeight
+
+        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        drawable.setBounds(0, 0, canvas.width, canvas.height)
+        drawable.draw(canvas)
+
+        return bitmap
+    }
+}
